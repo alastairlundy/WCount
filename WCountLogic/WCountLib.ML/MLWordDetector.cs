cbf4e14f3c99f46484e76bfde2648e6939e76993
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-05 19:35:06 +0000

    move WCount Logic and WCount API to new folders

---
+++ WCountLogic/WCountLib.ML/MLWordDetector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+using AlastairLundy.Extensions.System.Strings.Contains;
+
+using WCountLib.Abstractions;
+// ReSharper disable RedundantBoolCompare
+
+namespace WCountLib.ML;
+
+public class MLWordDetector : IWordDetector
+{
+    
+    public bool IsStringAWord(string s, bool excludeStringsWithSpaces = true)
+    {
+        return
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="delimitersToExclude"></param>
+    /// <param name="excludeStringsWithSpaces"></param>
+    /// <returns></returns>
+    public bool IsStringAWord(string s, IEnumerable<char> delimitersToExclude, bool excludeStringsWithSpaces = true)
+    {
+        if (string.IsNullOrWhiteSpace(s) == true)
+        {
+            return false;
+        }
+
+        if (s.ContainsAnyOf(delimitersToExclude) == true)
+        {
+            return false;
+        }
+        
+        return IsStringAWord(s, excludeStringsWithSpaces);
+    }
+}
