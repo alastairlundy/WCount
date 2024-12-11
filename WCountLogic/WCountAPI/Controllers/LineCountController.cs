55db3690efd14725542bf2a7025adb2e0a5e0728
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-11 11:03:35 +0000

    add LineCounting to API

---
+++ WCountLogic/WCountAPI/Controllers/LineCountController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using WCountLib.Abstractions.Counters;
+
+namespace WCountAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LineCountController : ControllerBase
+{
+    private readonly ILineCounter _lineCounter;
+
+    public LineCountController(ILineCounter lineCounter)
+    {
+       _lineCounter = lineCounter;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    [Route("/string")]
+    [HttpPost]
+    public int CountLines([FromBody] string s)
+    {
+        return _lineCounter.CountLines(s);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="strings"></param>
+    /// <returns></returns>
+    [Route("/strings")]
+    [HttpPost]
+    public int CountLines([FromBody] IEnumerable<string> strings)
+    {
+        return _lineCounter.CountLines(strings);
+    }
+}
