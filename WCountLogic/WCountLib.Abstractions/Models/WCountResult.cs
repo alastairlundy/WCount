e86902203ade7602cd81d62bf78d5263f7b5b0cb
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-11 14:40:07 +0000

    re-add WCountResult model

---
+++ WCountLogic/WCountLib.Abstractions/Models/WCountResult.cs
@@ -0,0 +1,19 @@
+﻿/*
+    WCountLib
+    Copyright (C) 2024 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+namespace WCountLib.Abstractions.Models
+{
+    public class WCountResult
+    {
+        public ulong CharCount { get; protected set; }
+        public ulong WordCount { get; protected set; }
+        public ulong ByteCount { get; protected set; }
+        public int LineCount { get; protected set; }
+    }
+}
