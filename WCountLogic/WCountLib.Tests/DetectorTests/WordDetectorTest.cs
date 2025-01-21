c498dcf86b698261e67f1df0132cce3bf15f5a02
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-21 10:53:36 +0000

    start work on unit testing

---
+++ WCountLogic/WCountLib.Tests/DetectorTests/WordDetectorTest.cs
@@ -0,0 +1,21 @@
+﻿using WCountLib.Detectors;
+using WCountLib.Detectors.Abstractions;
+
+namespace WCountLib.Tests.DetectorTests;
+
+public class WordDetectorTest
+{
+    private readonly IWordDetector _wordDetector = new WordDetector();
+
+    [Theory]
+    public void BasicIsStringAWordTest(string s)
+    {
+           
+    }
+
+    [Theory]
+    public void AdvancedIsStringAWordTest(string s)
+    {
+        
+    }
+}
