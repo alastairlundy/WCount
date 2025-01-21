1aa444310340b8970cb5014bf1106d817011e989
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-21 10:52:45 +0000

    Create DependencyInjectionExtensions.cs

---
+++ WCountLogic/WCountLib/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿/*
+    WCountLib
+    Copyright (C) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using Microsoft.Extensions.DependencyInjection;
+
+using WCountLib.Counters;
+using WCountLib.Counters.Abstractions;
+using WCountLib.Counters.Abstractions.IO.Specializations;
+using WCountLib.Counters.IO;
+using WCountLib.Detectors;
+using WCountLib.Detectors.Abstractions;
+// ReSharper disable UnusedMember.Global
+
+namespace WCountLib.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection UseWCount(this IServiceCollection services)
+        {
+            services.AddSingleton<IWordDetector, WordDetector>();
+            
+            services.AddSingleton<ILineCounter, LineCounter>();
+            services.AddSingleton<IByteCounter, ByteCounter>();
+            services.AddSingleton<ICharCounter, CharCounter>();
+            services.AddSingleton<IWordCounter, WordCounter>();
+
+            services.AddTransient<ITextFileCounter, TextFileCounter>();
+
+            return services;
+        }
+    }
+}
