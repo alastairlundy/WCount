3ad34f7fd777eb0b94d7f8fe44bc46be0db511f5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-21 10:52:38 +0000

    add new abstraction

---
+++ WCountLogic/WCountLib/Counters/Abstractions/IO/Specializations/ITextFileCounter.cs
@@ -0,0 +1,16 @@
+﻿/*
+    WCountLib
+    Copyright (C) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+namespace WCountLib.Counters.Abstractions.IO.Specializations
+{
+    public interface ITextFileCounter : IFileLineCounter, IFileByteCounter, IFileCharCounter, IFileWordCounter
+    {
+        public bool IsATextFile(string filePath);
+    }
+}
