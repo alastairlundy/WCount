77f807afc36bed0d8a3df223a8519a2c9ebc2b35
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-05 15:35:33 +0000

    note future work todo

--- WCountLogic/WCountLib.Abstractions/Counters/ICharacterCounter.cs
+++ WCountLogic/WCountLib.Abstractions/Counters/ICharacterCounter.cs
@@ -29,6 +29,10 @@ namespace AlastairLundy.WCountLib.Abstractions.Counters
         /// <returns>The total number of characters counted.</returns>
         int CountCharacters(TextReader textReader, Encoding textEncodingType);
         
+        // TODO: Add ulong non-async interface method in v4
+
+        // TODO: Add int async interface method in v4
+
         /// <summary>
         /// Asynchronously reads from the provided TextReader and counts the total number of characters in the specified Encoding.
         /// </summary>
