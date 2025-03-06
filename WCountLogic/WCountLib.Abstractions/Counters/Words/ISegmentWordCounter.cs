c21053a3e82787f7a89a44501c2132241e27f481
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-06 12:07:10 +0000

    add missing license notice

--- WCountLogic/WCountLib.Abstractions/Counters/Words/ISegmentWordCounter.cs
+++ WCountLogic/WCountLib.Abstractions/Counters/Words/ISegmentWordCounter.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections.Generic;
+﻿/*
+    WCountLib.Abstraction
+    Copyright (C) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+
+using System.Collections.Generic;
+
 using Microsoft.Extensions.Primitives;
 
 namespace AlastairLundy.WCountLib.Abstractions.Counters.Words;
