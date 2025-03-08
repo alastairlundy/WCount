fbe66de73ff83a53ccfd7ac5a4507cf4b782c883
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-08 11:23:30 +0000

    start working on boilerplate for Spectre DI code

---
+++ WCountUI/WCount.Cli/DependencyInjection/TypeRegistrar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+namespace WCount.Cli.DependencyInjection;
+
+internal class TypeRegistrar : ITypeRegistrar
+{
+    private readonly IServiceCollection _serviceCollection;
+    
+    internal TypeRegistrar(IServiceCollection serviceCollection)
+    {
+        _serviceCollection = serviceCollection;
+    }
+    
+    public void Register(Type service, Type implementation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RegisterInstance(Type service, object implementation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RegisterLazy(Type service, Func<object> factory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ITypeResolver Build()
+    {
+        throw new NotImplementedException();
+    }
+}
