238e58e930ff136c98bd258ce0d61cb7e5282a94
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-31 13:08:02 +0100

    create ISegmentCharCounter

---
+++ WCountLogic/WCountLib.Abstractions/Counters/Characters/ISegmentCharCounter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+
+namespace AlastairLundy.WCountLib.Abstractions.Counters.Characters
+{
+    public interface ISegmentCharacterCounter
+    {
+        int CountCharactersInt32(IEnumerable<StringSegment> segments);
+    
+        ulong CountCharactersUInt64(IEnumerable<StringSegment> segments);
+    }
+}
