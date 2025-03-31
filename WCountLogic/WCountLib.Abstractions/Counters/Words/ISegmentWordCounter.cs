3616d24a8bf227d4772c96a6cf08d08a62b332e0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-31 13:07:40 +0100

    Update ISegmentWordCounter.cs

--- WCountLogic/WCountLib.Abstractions/Counters/Words/ISegmentWordCounter.cs
+++ WCountLogic/WCountLib.Abstractions/Counters/Words/ISegmentWordCounter.cs
@@ -12,11 +12,12 @@ using System.Collections.Generic;
 
 using Microsoft.Extensions.Primitives;
 
-namespace AlastairLundy.WCountLib.Abstractions.Counters.Words;
-
-public interface IStringSegmentWordCounter
+namespace AlastairLundy.WCountLib.Abstractions.Counters.Words
 {
-    int CountWordsInt32(IEnumerable<StringSegment> segments);
+    public interface IStringSegmentWordCounter
+    {
+        int CountWordsInt32(IEnumerable<StringSegment> segments);
     
-    ulong CountWordsUInt64(IEnumerable<StringSegment> segments);
+        ulong CountWordsUInt64(IEnumerable<StringSegment> segments);
+    }
 }
