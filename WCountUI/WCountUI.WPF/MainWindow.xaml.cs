de33f72018e6da00191213190d079ba83d87778d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-01 22:39:42 +0100

    start working on code powering main form

--- WCountUI/WCountUI.WPF/MainWindow.xaml.cs
+++ WCountUI/WCountUI.WPF/MainWindow.xaml.cs
@@ -1,24 +1,79 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using AlastairLundy.WCountLib.Abstractions.Counters;
 
 namespace WCountUI.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public MainWindow()
+        private readonly IWordCounter _wordCounter;
+        private readonly ICharacterCounter _characterCounter;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Text { get; set; }
+
+        public ulong WordCount { get; set; }
+        public int CharacterCount { get; set; }
+
+        public string WordCountText
+        {
+            get
+            {
+                if(WordCount == 1)
+                {
+                    return  Localizations.Resources.Labels_Words_Singular;
+                }
+                else
+                {
+                    return Localizations.Resources.Labels_Words_Plural;
+                }
+            }
+        }
+
+        public string CharacterCountText
+        {
+            get
+            {
+                if (CharacterCount == 1)
+                {
+                    return Localizations.Resources.Labels_Characters_Singular;
+                }
+                else
+                {
+                    return Localizations.Resources.Labels_Characters_Plural;
+                }
+            }
+        }
+
+        public MainWindow(IWordCounter wordCounter, ICharacterCounter characterCounter)
         {
             InitializeComponent();
+            Title = Localizations.Resources.App_Labels_Name;
+
+            _wordCounter = wordCounter;
+            _characterCounter = characterCounter;
+
+            Text = string.Empty;
+            WordCount = 0;
+            CharacterCount = 0; 
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Task task = Task.Run(() => {
+                WordCount = _wordCounter.CountWords(new StringReader(Text));
+                CharacterCount = _characterCounter.CountCharacters(new StringReader(Text), Encoding.Default);
+            });
         }
     }
 }
