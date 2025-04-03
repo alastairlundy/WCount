f4ae089e9a26a9cbbb0f606bda2293b19e37340e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-03 18:32:12 +0100

    Update dependencies

--- WCountLogic/WCountLib/Detectors/WordDetector.cs
+++ WCountLogic/WCountLib/Detectors/WordDetector.cs
@@ -10,7 +10,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-using AlastairLundy.Extensions.System.Strings;
+using AlastairLundy.DotExtensions.Strings;
 
 using AlastairLundy.WCountLib.Abstractions.Detectors;
 
