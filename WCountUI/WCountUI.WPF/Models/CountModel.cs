418995cc4952942a24bd1add419dff773fd12b5f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-03 12:47:11 +0100

    Create CountModel.cs

---
+++ WCountUI/WCountUI.WPF/Models/CountModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCountUI.WPF.Models
+{
+    public class CountModel : INotifyPropertyChanged
+    {
+        private ulong wordCount;
+        private int characterCount;
+
+        public CountModel()
+        {
+          
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
