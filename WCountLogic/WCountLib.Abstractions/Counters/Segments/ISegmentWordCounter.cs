72acc6d5ac6460cbd4db05f6068994f57f99ff6d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-03 18:16:40 +0100

    add spacing between using namespaces

--- WCountLogic/WCountLib.Abstractions/Counters/Segments/ISegmentWordCounter.cs
+++ WCountLogic/WCountLib.Abstractions/Counters/Segments/ISegmentWordCounter.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using Microsoft.Extensions.Primitives;
 
 namespace AlastairLundy.WCountLib.Abstractions.Counters.Segments
