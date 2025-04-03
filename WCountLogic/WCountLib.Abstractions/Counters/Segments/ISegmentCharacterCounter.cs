d52e09801bfe3c09f62e116593bbb7c3165b687b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-03 12:56:14 +0100

    rename segment char counter

---
+++ WCountLogic/WCountLib.Abstractions/Counters/Segments/ISegmentCharacterCounter.cs
@@ -0,0 +1,21 @@
+﻿/*
+    WCountLib.Abstraction
+    Copyright (C) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+
+namespace AlastairLundy.WCountLib.Abstractions.Counters.Segments
+{
+    public interface ISegmentCharacterCounter
+    {
+        int CountCharactersInt32(IEnumerable<StringSegment> segments);
+    
+        ulong CountCharactersUInt64(IEnumerable<StringSegment> segments);
+    }
+}
