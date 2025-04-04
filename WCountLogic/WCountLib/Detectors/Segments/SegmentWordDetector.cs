bb69d0d7b982f64cd0c3526794c0afd6ad98ee90
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-05 00:35:57 +0100

    implement Segmented Word Detector

--- WCountLogic/WCountLib/Detectors/Segments/SegmentWordDetector.cs
+++ WCountLogic/WCountLib/Detectors/Segments/SegmentWordDetector.cs
@@ -7,21 +7,29 @@
     file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
+
+using AlastairLundy.DotExtensions.MsExtensions.System.StringSegments;
 using AlastairLundy.DotExtensions.Strings;
 using AlastairLundy.WCountLib.Abstractions.Detectors.Segments;
 
 using Microsoft.Extensions.Primitives;
+// ReSharper disable RedundantBoolCompare
 
 namespace AlastairLundy.WCountLib.Detectors.Segments;
 
 public class SegmentWordDetector : ISegmentWordDetector
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="segment"></param>
+    /// <param name="countStringsWithSpacesAsWords"></param>
+    /// <returns></returns>
     public bool IsStringAWord(StringSegment segment, bool countStringsWithSpacesAsWords = false)
     {
-        bool containsSpaceSeparatedSubStrings = segment.Split([' ']).Count() > 1 == false;
+        bool containsSpaceSeparatedSubStrings = segment.Contains(' ') == false;
 
         bool output = containsSpaceSeparatedSubStrings;
         
@@ -31,14 +39,12 @@ public class SegmentWordDetector : ISegmentWordDetector
             output = false;
         }
         
-        ReadOnlySpan<char> span = segment.AsSpan();
-        
-        if(span.ToArray().All(c => c.IsSpecialCharacter() == true))
+        if(segment.ToCharArray().All(c => c.IsSpecialCharacter() == true))
         {
             output = false;
         }
 
-        if (segment.Split([' ']).Count() == 1 && countStringsWithSpacesAsWords == false)
+        if (containsSpaceSeparatedSubStrings && countStringsWithSpacesAsWords == false)
         {
             output = true;
         }
@@ -46,8 +52,29 @@ public class SegmentWordDetector : ISegmentWordDetector
         return output;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="segment"></param>
+    /// <param name="delimitersToExclude"></param>
+    /// <param name="countStringsWithSpacesAsWords"></param>
+    /// <returns></returns>
     public bool IsStringAWord(StringSegment segment, IEnumerable<char> delimitersToExclude, bool countStringsWithSpacesAsWords = false)
     {
-        throw new System.NotImplementedException();
+        bool containsSpaceSeparatedSubstrings = segment.Contains(' ');
+        bool output = false;
+            
+        if (StringSegment.IsNullOrEmpty(segment) == false ||
+            containsSpaceSeparatedSubstrings && countStringsWithSpacesAsWords == false)
+        {
+            output = false;    
+        }
+
+        if (delimitersToExclude.Select(x => segment.ToCharArray().Contains(x)).Any() == true)
+        {
+            output = false;
+        }
+
+        return output;
     }
 }
