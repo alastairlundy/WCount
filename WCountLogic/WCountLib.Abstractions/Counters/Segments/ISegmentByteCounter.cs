dd1069832bdcac31a9e91a1662a0f2762e05a644
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-06 17:17:13 +0100

    add xml doc comments to 3.2 abstractions

--- WCountLogic/WCountLib.Abstractions/Counters/Segments/ISegmentByteCounter.cs
+++ WCountLogic/WCountLib.Abstractions/Counters/Segments/ISegmentByteCounter.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using Microsoft.Extensions.Primitives;
 
 namespace AlastairLundy.WCountLib.Abstractions.Counters.Segments
@@ -18,31 +19,27 @@ namespace AlastairLundy.WCountLib.Abstractions.Counters.Segments
     public interface ISegmentByteCounter
     {
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="segments"></param>
-        /// <returns></returns>
+        /// Returns the total number of bytes that can be represented by 32-bit integers in all segments. </summary>
+        /// <param name="segments">The segments to count bytes from.</param>
+        /// <returns>The total number of bytes that can be represented by 32-bit integers in all segments.</returns>
         int CountBytesInt32(IEnumerable<StringSegment> segments);
-    
+
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="segments"></param>
-        /// <returns></returns>
+        /// Returns the total number of bytes that can be represented by 64-bit unsigned integers in all segments. </summary>
+        /// <param name="segments">The segments to count bytes from.</param>
+        /// <returns>The total number of bytes that can be represented by 64-bit unsigned integers in all segments.</returns>
         UInt64 CountBytesUInt64(IEnumerable<StringSegment> segments);
-        
+
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="segments"></param>
-        /// <returns></returns>
+        /// Returns the total number of bytes (in units of 32-bit integers) in all segments. This method is asynchronous to allow for non-blocking operations and efficient use of threads. </summary>
+        /// <param name="segments">The segments to count bytes from.</param>
+        /// <returns>The total number of bytes (in units of 32-bit integers) in all segments.</returns>
         Task<int> CountBytesInt32Async(IEnumerable<StringSegment> segments);
-        
+
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="segments"></param>
-        /// <returns></returns>
+        /// Returns the total number of bytes (in units of 64-bit unsigned integers) in all segments. This method is asynchronous to allow for non-blocking operations and efficient use of threads. </summary>
+        /// <param name="segments">The segments to count bytes from.</param>
+        /// <returns>The total number of bytes (in units of 64-bit unsigned integers) in all segments.</returns>
         Task<UInt64> CountBytesUInt64Async(IEnumerable<StringSegment> segments);
     }
 }
