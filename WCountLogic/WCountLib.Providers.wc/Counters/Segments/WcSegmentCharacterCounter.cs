7fcb45d37b2d415db1df4c98c3df14086967a9a1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-27 15:48:33 +0100

    add missing license notices

--- WCountLogic/WCountLib.Providers.wc/Counters/Segments/WcSegmentCharacterCounter.cs
+++ WCountLogic/WCountLib.Providers.wc/Counters/Segments/WcSegmentCharacterCounter.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections.Generic;
+﻿/*
+    WCountLib.Providers.wc
+    Copyright (C) 2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AlastairLundy.WCountLib.Abstractions.Counters.Segments;
 using Microsoft.Extensions.Primitives;
