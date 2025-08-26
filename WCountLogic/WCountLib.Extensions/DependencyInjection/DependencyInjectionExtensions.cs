01158ec5d11f74c985b802c8d09cdffa6c619b25
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-26 17:48:32 +0100

    Update DependencyInjectionExtensions.cs

--- WCountLogic/WCountLib.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
+++ WCountLogic/WCountLib.Extensions/DependencyInjection/DependencyInjectionExtensions.cs
@@ -20,65 +20,64 @@ using Microsoft.Extensions.DependencyInjection;
 
 // ReSharper disable UnusedMember.Global
 
-namespace AlastairLundy.WCountLib.Extensions.DependencyInjection
+namespace AlastairLundy.WCountLib.Extensions.DependencyInjection;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    /// <summary>
+    /// Sets up the required services and implementations for Dependency Injection using Microsoft.Extensions.DependencyInjection.
+    /// </summary>
+    /// <param name="services">The service collection to add to.</param>
+    /// <param name="lifetime">The service lifetime to register the services as.</param>
+    /// <returns>The updated ServiceCollection with WCount's services.</returns>
+    public static IServiceCollection AddWCount(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
     {
-        /// <summary>
-        /// Sets up the required services and implementations for Dependency Injection using Microsoft.Extensions.DependencyInjection.
-        /// </summary>
-        /// <param name="services">The service collection to add to.</param>
-        /// <param name="lifetime">The service lifetime to register the services as.</param>
-        /// <returns>The updated ServiceCollection with WCount's services.</returns>
-        public static IServiceCollection AddWCount(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        switch (lifetime)
         {
-            switch (lifetime)
-            {
-                case ServiceLifetime.Scoped:
-                    services.AddScoped<ISegmentWordDetector, SegmentWordDetector>();
-                    services.AddScoped<ISegmentByteCounter, SegmentByteCounter>();
-                    services.AddScoped<ISegmentCharacterCounter, SegmentCharacterCounter>();
-                    services.AddScoped<ISegmentLineCounter, SegmentLineCounter>();
-                    services.AddScoped<ISegmentWordCounter, SegmentWordCounter>();
+            case ServiceLifetime.Scoped:
+                services.AddScoped<ISegmentWordDetector, SegmentWordDetector>();
+                services.AddScoped<ISegmentByteCounter, SegmentByteCounter>();
+                services.AddScoped<ISegmentCharacterCounter, SegmentCharacterCounter>();
+                services.AddScoped<ISegmentLineCounter, SegmentLineCounter>();
+                services.AddScoped<ISegmentWordCounter, SegmentWordCounter>();
                     
-                    services.AddScoped<IWordDetector, WordDetector>();
+                services.AddScoped<IWordDetector, WordDetector>();
                     
-                    services.AddScoped<ILineCounter, LineCounter>();
-                    services.AddScoped<IByteCounter, ByteCounter>();
-                    services.AddScoped<ICharacterCounter, CharacterCounter>();
-                    services.AddScoped<IWordCounter, WordCounter>();
-                    break;
-                case ServiceLifetime.Singleton:
-                    services.AddSingleton<ISegmentWordDetector, SegmentWordDetector>();
-                    services.AddSingleton<ISegmentByteCounter, SegmentByteCounter>();
-                    services.AddSingleton<ISegmentCharacterCounter, SegmentCharacterCounter>();
-                    services.AddSingleton<ISegmentLineCounter, SegmentLineCounter>();
-                    services.AddSingleton<ISegmentWordCounter, SegmentWordCounter>();
+                services.AddScoped<ILineCounter, LineCounter>();
+                services.AddScoped<IByteCounter, ByteCounter>();
+                services.AddScoped<ICharacterCounter, CharacterCounter>();
+                services.AddScoped<IWordCounter, WordCounter>();
+                break;
+            case ServiceLifetime.Singleton:
+                services.AddSingleton<ISegmentWordDetector, SegmentWordDetector>();
+                services.AddSingleton<ISegmentByteCounter, SegmentByteCounter>();
+                services.AddSingleton<ISegmentCharacterCounter, SegmentCharacterCounter>();
+                services.AddSingleton<ISegmentLineCounter, SegmentLineCounter>();
+                services.AddSingleton<ISegmentWordCounter, SegmentWordCounter>();
                     
-                    services.AddSingleton<IWordDetector, WordDetector>();
+                services.AddSingleton<IWordDetector, WordDetector>();
                     
-                    services.AddSingleton<ILineCounter, LineCounter>();
-                    services.AddSingleton<IByteCounter, ByteCounter>();
-                    services.AddSingleton<ICharacterCounter, CharacterCounter>();
-                    services.AddSingleton<IWordCounter, WordCounter>();
-                    break;
-                case ServiceLifetime.Transient:
-                    services.AddTransient<ISegmentWordDetector, SegmentWordDetector>();
-                    services.AddTransient<ISegmentByteCounter, SegmentByteCounter>();
-                    services.AddTransient<ISegmentCharacterCounter, SegmentCharacterCounter>();
-                    services.AddTransient<ISegmentLineCounter, SegmentLineCounter>();
-                    services.AddTransient<ISegmentWordCounter, SegmentWordCounter>();
+                services.AddSingleton<ILineCounter, LineCounter>();
+                services.AddSingleton<IByteCounter, ByteCounter>();
+                services.AddSingleton<ICharacterCounter, CharacterCounter>();
+                services.AddSingleton<IWordCounter, WordCounter>();
+                break;
+            case ServiceLifetime.Transient:
+                services.AddTransient<ISegmentWordDetector, SegmentWordDetector>();
+                services.AddTransient<ISegmentByteCounter, SegmentByteCounter>();
+                services.AddTransient<ISegmentCharacterCounter, SegmentCharacterCounter>();
+                services.AddTransient<ISegmentLineCounter, SegmentLineCounter>();
+                services.AddTransient<ISegmentWordCounter, SegmentWordCounter>();
                     
-                    services.AddTransient<IWordDetector, WordDetector>();
+                services.AddTransient<IWordDetector, WordDetector>();
                     
-                    services.AddTransient<ILineCounter, LineCounter>();
-                    services.AddTransient<IByteCounter, ByteCounter>();
-                    services.AddTransient<ICharacterCounter, CharacterCounter>();
-                    services.AddTransient<IWordCounter, WordCounter>();
-                    break;
-            }
-            
-            return services;
+                services.AddTransient<ILineCounter, LineCounter>();
+                services.AddTransient<IByteCounter, ByteCounter>();
+                services.AddTransient<ICharacterCounter, CharacterCounter>();
+                services.AddTransient<IWordCounter, WordCounter>();
+                break;
         }
+            
+        return services;
     }
 }
