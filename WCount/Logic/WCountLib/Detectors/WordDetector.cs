f218ca74e40f744e535f525aabaa3f54794cfe15
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-12 18:24:49 +0100

    Update WordDetector.cs

--- WCount/Logic/WCountLib/Detectors/WordDetector.cs
+++ WCount/Logic/WCountLib/Detectors/WordDetector.cs
@@ -7,8 +7,6 @@
     file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 
 using AlastairLundy.DotExtensions.Strings;
