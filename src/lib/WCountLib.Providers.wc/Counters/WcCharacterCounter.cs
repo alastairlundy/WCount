e0bc6e3e984fa67d81cb84ce27e0adef030a10c0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-04-26 00:01:29 +0100

    update copyright year

--- src/lib/WCountLib.Providers.wc/Counters/WcCharacterCounter.cs
+++ src/lib/WCountLib.Providers.wc/Counters/WcCharacterCounter.cs
@@ -1,6 +1,6 @@
 ﻿/*
     WCountLib.Providers.wc
-    Copyright (C) 2025 Alastair Lundy
+    Copyright (C) 2026 Alastair Lundy
 
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
--- src/lib/WCountLib/Counters/Segments/SegmentCharacterCounter.cs
+++ src/lib/WCountLib/Counters/Segments/SegmentCharacterCounter.cs
@@ -1,6 +1,6 @@
 ﻿/*
     WCountLib
-    Copyright (C) 2024-2025 Alastair Lundy
+    Copyright (C) 2024-2026 Alastair Lundy
 
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
