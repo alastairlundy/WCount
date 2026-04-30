75e1edf7fc2c2b1d8e68e9e132f851f12ae8eeed
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-04-30 11:38:45 +0100

    add license headers

--- src/WCountCli/Helpers/FormattingHelpers.cs
+++ src/WCountCli/Helpers/FormattingHelpers.cs
@@ -1,4 +1,13 @@
-﻿namespace WCountCli.Helpers;
+﻿/*
+    WCount Cli
+    Copyright (C) 2026 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+namespace WCountCli.Helpers;
 
 public static class FormattingHelpers
 {
